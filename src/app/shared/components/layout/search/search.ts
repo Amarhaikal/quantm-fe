1def354f93ae2673d221d05b125612ad5df9d072
Amar Haikal <muhdamarhaikal@gmail.com>
2026-02-21 02:03:54 +0800

    search: set 4 column

--- src/app/shared/components/layout/search/search.ts
+++ src/app/shared/components/layout/search/search.ts
@@ -11,7 +11,7 @@ import { CardComponent } from '../card/card';
     <lib-card [padding]="true">
       <div class="flex flex-col md:flex-row items-stretch md:items-end gap-4">
         <div class="flex-1 w-full">
-          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             <ng-content></ng-content>
           </div>
         </div>
