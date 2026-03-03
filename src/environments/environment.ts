5129b7eeea50983713744c2f3196caf6842a37c6
Amar Haikal <muhdamarhaikal@gmail.com>
2026-03-03 12:46:32 +0800

    change url 217 to quantm-bank.com

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://217.217.255.210/api',
+  apiUrl: 'https://quantm-bank.com/api',
   azureAd: {
     clientId: 'a1b425c3-6265-4580-a417-0b8c74f77e8e',
     authority: 'https://login.microsoftonline.com/6bfecc36-0624-428f-b3bf-84cc04589b28',
-    redirectUri: 'http://217.217.255.210/auth/login',
+    redirectUri: 'https://quantm-bank.com/auth/login',
   },
 };
