a394ae44e96a5431978a2dd60093fdf8a706bb31
Amar Haikal <muhdamarhaikal@gmail.com>
2026-03-10 00:18:19 +0800

    chore: Update APM server URL and environment to production.

--- src/app/core/apm.config.ts
+++ src/app/core/apm.config.ts
@@ -2,11 +2,11 @@ import { init as initApm } from '@elastic/apm-rum';
 
 export const apm = initApm({
   serviceName: 'quantm-fe',
-  serverUrl: 'http://localhost:8200',
+  serverUrl: 'http://217.217.255.210:8200',
   serviceVersion: '1.0.0',
-  environment: 'development',
+  environment: 'production',
   distributedTracingOrigins: [
-    'http://localhost:8080', // Spring Boot Primary
+    'http://localhost:8080', // Spring Boot Primary (can keep for local dev)
     'http://localhost:8081', // Spring Boot Secondary
     'http://localhost:8000', // Python AI Service
     'https://quantm-bank.com', // Production domain
